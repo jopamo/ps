9130e70abc3da3680956ac66b51c076567f660f4
jopamo <p@1g4.org>
2025-02-03 03:59:46 -0500

    Initial skel files

---
+++ oss.c
@@ -0,0 +1,9 @@
+/* * oss.c (Skeleton) */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    printf("OSS Skeleton: Hello from oss!\n");
+    return 0;
+}
---
+++ user.c
@@ -0,0 +1,9 @@
+/* * user.c (Skeleton) */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    printf("USER Skeleton: Hello from user!\n");
+    return 0;
+}
