e7e751b8a27252b0efbb038890e16482b0851ec2
jopamo <p@1g4.org>
2025-02-10 03:21:40 -0500

    update format

--- p1/user.c
+++ p1/user.c
@@ -14,39 +14,37 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int parseIterations(const char *arg)
-{
-    int val = atoi(arg);
-    if (val <= 0) {
-        return -1;
-    }
-    return val;
+int parseIterations(const char * arg) {
+  int val = atoi(arg);
+  if (val <= 0) {
+    return -1;
+  }
+  return val;
 }
 
 #ifndef TESTING
-int main(int argc, char *argv[])
-{
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <iterations>\n", argv[0]);
-        return 1;
-    }
+int main(int argc, char * argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <iterations>\n", argv[0]);
+    return 1;
+  }
 
-    int iterations = parseIterations(argv[1]);
-    if (iterations < 0) {
-        fprintf(stderr, "Error: iterations must be positive\n");
-        return 1;
-    }
+  int iterations = parseIterations(argv[1]);
+  if (iterations < 0) {
+    fprintf(stderr, "Error: iterations must be positive\n");
+    return 1;
+  }
 
-    pid_t pid = getpid();
-    pid_t ppid = getppid();
+  pid_t pid = getpid();
+  pid_t ppid = getppid();
 
-    for (int i = 1; i <= iterations; i++) {
-        printf("USER PID:%d PPID:%d Iteration:%d before sleeping\n", pid, ppid, i);
-        fflush(stdout);
-        sleep(1);
-        printf("USER PID:%d PPID:%d Iteration:%d after sleeping\n", pid, ppid, i);
-        fflush(stdout);
-    }
-    return 0;
+  for (int i = 1; i <= iterations; i++) {
+    printf("USER PID:%d PPID:%d Iteration:%d before sleeping\n", pid, ppid, i);
+    fflush(stdout);
+    sleep(1);
+    printf("USER PID:%d PPID:%d Iteration:%d after sleeping\n", pid, ppid, i);
+    fflush(stdout);
+  }
+  return 0;
 }
 #endif
